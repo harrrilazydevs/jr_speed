20a37438162f4e4e3e678aaa9201560dd37ac8e6
harrrilazydevs <86334894+harrrilazydevs@users.noreply.github.com>
2023-04-22 23:19:00 +0800

    test

--- index.php
+++ index.php
@@ -12,92 +12,105 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary banner">
+    <div id="panel_user">
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary banner">
+
+            <div class="container">
+
+                <div class="navbar-brand logo" href="#">
+
+                    <img src="src/img/jrspeed.png" width="40">
+
+                    <span style="vertical-align: middle;">JRSPEED</span>
+
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav">
+
+                    <span class="navbar-toggler-icon"></span>
+
+                </button>
+
+                <div class="collapse navbar-collapse ms-lg-5" id="navbarNav">
+
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item">
+
+                            <a id="btn_goto_home" class="nav-link text-light active" aria-current="page" href="#">HOME</a>
+
+                        </li>
+
+                        <li class="nav-item">
+
+                            <a id="btn_goto_shop" class="nav-link text-light" href="#">SHOP</a>
+
+                        </li>
+
+                        <li class="nav-item">
+
+                            <a id="btn_goto_contact" class="nav-link text-light" href="#">CONTACT</a>
+
+                        </li>
+
+                        <li class="nav-item">
+
+                            <a id="btn_goto_faq" class="nav-link text-light" href="#">FAQ</a>
+
+                        </li>
+
+                    </ul>
+
+                </div>
+
+                <button class="btn btn-primary d-none d-lg-block">Login</button>
+
+            </div>
+        </nav>
 
         <div class="container">
 
-            <div class="navbar-brand logo" href="#">
-
-                <img src="src/img/jrspeed.png" width="40">
-
-                <span style="vertical-align: middle;">JRSPEED</span>
-
+            <div class="row page" id="div_home">
+                <div class="col p-2 border mt-3">
+                    HOME PAGE
+                </div>
             </div>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav">
-
-                <span class="navbar-toggler-icon"></span>
-
-            </button>
-
-            <div class="collapse navbar-collapse ms-lg-5" id="navbarNav">
-
-                <ul class="navbar-nav">
-
-                    <li class="nav-item">
-
-                        <a id="btn_goto_home" class="nav-link text-light active" aria-current="page" href="#">HOME</a>
-
-                    </li>
-
-                    <li class="nav-item">
-
-                        <a id="btn_goto_shop" class="nav-link text-light" href="#">SHOP</a>
-
-                    </li>
-
-                    <li class="nav-item">
-
-                        <a id="btn_goto_contact" class="nav-link text-light" href="#">CONTACT</a>
-
-                    </li>
-
-                    <li class="nav-item">
-
-                        <a id="btn_goto_faq" class="nav-link text-light" href="#">FAQ</a>
-
-                    </li>
-
-                </ul>
-
+            <div class="row page" style="display:none;" id="div_shop">
+                <div class="col p-2 border mt-3">
+                    SHOP PAGE
+                </div>
             </div>
 
-            <button class="btn btn-primary d-none d-lg-block">Login</button>
+            <div class="row page" style="display:none;" id="div_contact">
+                <div class="col p-2 border mt-3">
+                    CONTACT PAGE
+                </div>
+            </div>
+
+            <div class="row page" style="display:none;" id="div_faq">
+                <div class="col p-2 border mt-3">
+                    FAQ PAGE
+                </div>
+            </div>
 
         </div>
-    </nav>
 
-    <div class="container">
+    </div>
 
-        <div class="row page" id="div_home">
-            <div class="col p-2 border mt-3">
-                HOME PAGE
-            </div>
-        </div>
+    <div id="panel_admin" style="display:none;">
 
-        <div class="row page" style="display:none;" id="div_shop">
-            <div class="col p-2 border mt-3">
-                SHOP PAGE
-            </div>
-        </div>
-
-        <div class="row page" style="display:none;" id="div_contact">
-            <div class="col p-2 border mt-3">
-                CONTACT PAGE
-            </div>
-        </div>
-
-        <div class="row page" style="display:none;" id="div_faq">
-            <div class="col p-2 border mt-3">
-                FAQ PAGE
-            </div>
-        </div>
+        <?php
+        include 'admin.php';
+        ?>
 
     </div>
 
     <script src="src/bootstrap/bs.js"></script>
     <script src="src/jquery/jquery.js"></script>
     <script src="src/func/all.js"></script>
+    <script src="src/func/admin.js"></script>
 </body>
 
 </html>
